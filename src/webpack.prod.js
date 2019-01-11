0c9924b0fef2803460ef35b2388f130591ff5eee
LianJion <1995lian1002@gmail.com>
2019-01-11 14:57:34 +0800

    查看编译的状态

--- src/webpack.prod.js
+++ src/webpack.prod.js
@@ -9,6 +9,8 @@ logger.info('building for production...');
 
 const compiler = webpack(config, (err, stats) => {
   if (err) throw err;
+  consle.log('compile的状态显示');
+  console.log(status);
 });
 
 compiler.apply(new ProgressBarPlugin());
