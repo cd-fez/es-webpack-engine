f145a24500a88dfb1136582aff6739813acaa2a3
LianJion <1995lian1002@gmail.com>
2020-03-29 18:30:56 +0800

    撤回多余的改动

--- src/webpack.prod.js
+++ src/webpack.prod.js
@@ -1,6 +1,5 @@
 import webpack from 'webpack';
 import ProgressBarPlugin from 'progress-bar-webpack-plugin';
-import WebpackNotifierPlugin from 'es-webpack-notifier';
 import config from './webpack.base';
 
 import logger from './config/logger';
@@ -10,6 +9,5 @@ logger.info('building for production...');
 const compiler = webpack(config, (err, stats) => {
   if (err) throw err;
 });
-
 
 new ProgressBarPlugin().apply(compiler);
