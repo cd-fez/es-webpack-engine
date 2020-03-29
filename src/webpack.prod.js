33c373ab3da23e7202449469bb97d750491c55a0
LianJion <1995lian1002@gmail.com>
2020-03-29 14:59:19 +0800

    修改报错组件

--- src/webpack.prod.js
+++ src/webpack.prod.js
@@ -1,6 +1,7 @@
 import webpack from 'webpack';
 import ProgressBarPlugin from 'progress-bar-webpack-plugin';
 import WebpackNotifierPlugin from 'es-webpack-notifier';
+import ErrorPlugin from "webpack-notifier";
 import config from './webpack.base';
 
 import logger from './config/logger';
@@ -11,5 +12,6 @@ const compiler = webpack(config, (err, stats) => {
   if (err) throw err;
 });
 
-new WebpackNotifierPlugin().apply(compiler);
+new ErrorPlugin().apply(compiler);
+
 new ProgressBarPlugin().apply(compiler);
