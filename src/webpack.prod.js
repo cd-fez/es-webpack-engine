a6da6bf9916c506b42c377ec736549a052806a79
LianJion <1995lian1002@gmail.com>
2020-03-29 14:44:45 +0800

    编译报错监听

--- src/webpack.prod.js
+++ src/webpack.prod.js
@@ -1,6 +1,6 @@
 import webpack from 'webpack';
 import ProgressBarPlugin from 'progress-bar-webpack-plugin';
-
+import WebpackNotifierPlugin from 'es-webpack-notifier';
 import config from './webpack.base';
 
 import logger from './config/logger';
@@ -11,4 +11,5 @@ const compiler = webpack(config, (err, stats) => {
   if (err) throw err;
 });
 
+new WebpackNotifierPlugin().apply(compiler);
 new ProgressBarPlugin().apply(compiler);
